ff95c8160436108797b21c32cc265f7ef54d77ad
lzxm160 <lzxm160@sina.com>
2019-04-10 19:18:03 +0800

    x

--- rpcmethod/client_test.go
+++ rpcmethod/client_test.go
@@ -4,15 +4,27 @@
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
 // License 2.0 that can be found in the LICENSE file.
 
-package main
+package rpcmethod
 
 import (
 	"fmt"
+	"testing"
 
-    "github.com/iotexproject/iotex-core/protogen/iotextypes"
+	"github.com/stretchr/testify/require"
+
+	"github.com/iotexproject/iotex-core/protogen/iotexapi"
 )
 
-func main() {
-    var blk iotextypes.Block
-	fmt.Printf("Hello Antenna %s\n", blk.Header.ProducerPubkey)
+const(
+	host="http://127.0.0.1:14004"
+)
+func TestGetAccount(t *testing.T) {
+	require := require.New(t)
+	rpc,err:=NewRPCMethod(host)
+	require.NoError(err)
+	req:=iotexapi.GetAccountRequest{}
+	req.Address="io1mflp9m6hcgm2qcghchsdqj3z3eccrnekx9p0ms"
+	res,err:=rpc.GetAccount(&req)
+	require.NoError(err)
+	fmt.Println(res)
 }
