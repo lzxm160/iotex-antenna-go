65263c1ebd14952dbe5bd8e0429d6d1ffe5984da
lzxm160 <lzxm160@sina.com>
2019-04-17 17:58:35 +0800

    v

--- buildlibsecp256k1.sh
+++ buildlibsecp256k1.sh
@@ -6,6 +6,6 @@ sudo apt-get install autoconf -y
 sudo apt-get install default-jre -y
 cd vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/libsecp256k1
 ./autogen.sh
-./configure
+./configure --disable-shared --with-pic --with-bignum=no --enable-module-recovery --disable-jni
 make
 sudo make install
