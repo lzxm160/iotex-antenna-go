2aa19b19414d12ac1dd44579264ba597aded42f2
lzxm160 <lzxm160@sina.com>
2019-04-17 14:47:41 +0800

    source env

--- antenna/antenna.go
+++ antenna/antenna.go
@@ -4,18 +4,29 @@
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
 // License 2.0 that can be found in the LICENSE file.
 
-package utils
+package antenna
 
-import "math/big"
+import (
+	"math/big"
+
+	"github.com/iotexproject/iotex-antenna-go/iotx"
+	"github.com/iotexproject/iotex-antenna-go/utils"
+)
+
+// Antenna
+type Antenna = iotx.Iotx
+
+// NewRPCMethod returns RPCMethod interacting with endpoint
+func NewRPCMethod(endpoint string) (*Antenna, error) {
+	return iotx.NewRPCMethod(endpoint)
+}
 
 // FromRau is a function to convert Rau to Iotx.
 func FromRau(rau *big.Int) int64 {
-	rau = rau.Div(rau, big.NewInt(1e18))
-	return rau.Int64()
+	return utils.FromRau(rau)
 }
 
 // ToRau is a function to convert various units to Rau.
-func ToRau(iotx int64) *big.Int {
-	itx := big.NewInt(iotx)
-	return itx.Mul(itx, big.NewInt(1e18))
+func ToRau(iotxs int64) *big.Int {
+	return utils.ToRau(iotxs)
 }
