233191fed93c7a6fe9908417aef9203014926868
lzxm160 <lzxm160@sina.com>
2019-04-17 17:54:30 +0800

    v

--- buildlibsecp256k1.sh
+++ buildlibsecp256k1.sh
@@ -3,6 +3,7 @@ apt-get update
 sudo apt-get install openssl -y
 sudo apt-get install libssl-dev -y
 sudo apt-get install autoconf -y
+sudo apt-get install default-jre -y
 cd vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/libsecp256k1
 ./autogen.sh
 ./configure
