481346e16837cd5446bded8e29c037970f897bd0
lzxm160 <lzxm160@sina.com>
2019-04-17 18:14:12 +0800

    x

--- buildlibsecp256k1.sh
+++ buildlibsecp256k1.sh
@@ -4,7 +4,7 @@ sudo apt-get install openssl -y
 sudo apt-get install libssl-dev -y
 sudo apt-get install autoconf -y
 sudo apt-get install default-jre -y
-sudo apt-get install build-essential automake libtool pkg-config libffi-dev python-dev python-pip
+sudo apt-get install build-essential automake libtool pkg-config libffi-dev python-dev python-pip -y
 cd vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/libsecp256k1
 ./autogen.sh
 ./configure --disable-shared --with-pic --with-bignum=no --enable-module-recovery --disable-jni
