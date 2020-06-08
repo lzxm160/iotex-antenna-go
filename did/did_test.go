88f3409ed78b1e5071fdaab4d6d51d052c171f21
lzxm160 <lzxm160@sina.com>
2020-06-08 14:46:19 +0800

    x

--- did/did_test.go
+++ did/did_test.go
@@ -4,27 +4,26 @@
 //// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
 //// License 2.0 that can be found in the LICENSE file.
 //
-package test
+package main
 
-//import (
-//	"testing"
-//
-//	"github.com/iotexproject/iotex-antenna-go/v2/did"
-//	"github.com/iotexproject/iotex-antenna-go/v2/did/contract/IoTeXDID"
-//)
-//
-//const (
-//	sender                = "io1ph0u2psnd7muq5xv9623rmxdsxc4uapxhzpg02"
-//	privateKey            = "414efa99dfac6f4095d6954713fb0085268d400d6a05a8ae8a69b5b1c10b4bed"
-//	endpoint              = "api.testnet.iotex.one:443"
-//	IoTeXDID_address      = "io1eurq3lx4lzx9wdj56plw5rm59f5qzanacr3raz"
-//	IoTeXDIDProxy_address = "io1zgs5gqjl679qlj4gqqpa9t329r8f5gr8xc9lr0"
-//)
-//
-//func TestDidCreateDID(t *testing.T) {
-//	//require := require.New(t)
-//	did.CeateDID(endpoint, privateKey, IoTeXDIDProxy_address, IoTeXDID.IoTeXDIDABI, "1000000000000", uint64(1000000), "", "414efa99dfac6f4095d6954713fb0085268d400d6a05a8ae8a69b5b1c10b4bed", "url")
-//}
+import (
+	"testing"
+
+	"github.com/iotexproject/iotex-antenna-go/v2/did/contract/IoTeXDID"
+)
+
+const (
+	sender                = "io1ph0u2psnd7muq5xv9623rmxdsxc4uapxhzpg02"
+	privateKey            = "414efa99dfac6f4095d6954713fb0085268d400d6a05a8ae8a69b5b1c10b4bed"
+	endpoint              = "api.testnet.iotex.one:443"
+	IoTeXDID_address      = "io1eurq3lx4lzx9wdj56plw5rm59f5qzanacr3raz"
+	IoTeXDIDProxy_address = "io1zgs5gqjl679qlj4gqqpa9t329r8f5gr8xc9lr0"
+)
+
+func TestDidCreateDID(t *testing.T) {
+	//require := require.New(t)
+	CeateDID(endpoint, privateKey, IoTeXDIDProxy_address, IoTeXDID.IoTeXDIDABI, "1000000000000", uint64(1000000), "", "414efa99dfac6f4095d6954713fb0085268d400d6a05a8ae8a69b5b1c10b4bed", "url")
+}
 
 //
 //func TestDidDeleteDID(t *testing.T) {
