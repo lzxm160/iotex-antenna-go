e4bbaad6a9e8617180f68586a7cccb14bbf34145
lzxm160 <lzxm160@sina.com>
2020-06-26 10:05:44 +0800

    x

--- did/did_test.go
+++ did/did_test.go
@@ -34,8 +34,9 @@ const (
 	//signsender            = "io1vdtfpzkwpyngzvx7u2mauepnzja7kd5rryp0sg"
 	privateKey = "414efa99dfac6f4095d6954713fb0085268d400d6a05a8ae8a69b5b1c10b4bed"
 	//signPrivateKey        = "0d4d9b248110257c575ef2e8d93dd53471d9178984482817dcbd6edb607f8cc5"
-	endpoint         = "api.testnet.iotex.one:443"
-	IoTeXDID_address = "io1qswvysrfav83ehgsfsfa4v8ugw4vqfsydp8snk"
+	endpoint           = "api.testnet.iotex.one:443"
+	IoTeXDIDDB_address = "io1qswvysrfav83ehgsfsfa4v8ugw4vqfsydp8snk"
+	IoTeXDID_address   = "io1qswvysrfav83ehgsfsfa4v8ugw4vqfsydp8snk"
 	//IoTeXDIDProxy_address = "io1kqwtdw7daf74l8sf0u9j3u9fgv225ua6hjn3n0"
 )
 
@@ -45,6 +46,30 @@ var (
 	//stringTy, _  = abi.NewType("string")
 	//bytes32Ty, _ = abi.NewType("bytes32")
 )
+
+func TestDidStorage(t *testing.T) {
+	require := require.New(t)
+	conn, err := iotex.NewDefaultGRPCConn(endpoint)
+	require.NoError(err)
+	defer conn.Close()
+
+	acc, err := account.HexStringToAccount(privateKey)
+	require.NoError(err)
+	c := iotex.NewAuthedClient(iotexapi.NewAPIServiceClient(conn), acc)
+
+	data, err := hex.DecodeString(abibin.DIDStorageBin[2:])
+	require.NoError(err)
+
+	hash, err := c.DeployContract(data).SetGasPrice(big.NewInt(int64(unit.Qev))).SetGasLimit(10000000).Call(context.Background())
+	require.NoError(err)
+	require.NotNil(hash)
+	fmt.Println("hash", hex.EncodeToString(hash[:]))
+	time.Sleep(20 * time.Second)
+	receiptResponse, err := c.GetReceipt(hash).Call(context.Background())
+	contractAddress := receiptResponse.GetReceiptInfo().GetReceipt().GetContractAddress()
+	fmt.Println("Status:", receiptResponse.GetReceiptInfo().GetReceipt().Status)
+	fmt.Println("Contract Address:", contractAddress)
+}
 
 func TestDidDeployContract(t *testing.T) {
 	require := require.New(t)
